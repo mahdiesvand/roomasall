eeafb99ba0896ae9881d87a8921f5b5e2b954af6
mahdiesvand <mahdiesvand1020@gmail.com>
2026-08-03 10:29:10 +0330

    Update Firebase imports and add message handling

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { 
+getDatabase,
+ref,
+push,
+set,
+onValue
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyBmV0ikTOiXmtibjuXgJ4PhIbJcTxkhP5A",
   authDomain: "roomasal.firebaseapp.com",
@@ -13,10 +16,72 @@ const firebaseConfig = {
   projectId: "roomasal",
   storageBucket: "roomasal.firebasestorage.app",
   messagingSenderId: "74039115594",
-  appId: "1:74039115594:web:18fb78cfe77207f18c4bb3",
-  measurementId: "G-T9W22HHGBF"
+  appId: "1:74039115594:web:18fb78cfe77207f18c4bb3"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const database = getDatabase(app);
+
+
+
+window.sendMessage = function(){
+
+let name = document.getElementById("name").value;
+let text = document.getElementById("message").value;
+
+
+if(name==="" || text===""){
+alert("نام و پیام را وارد کنید");
+return;
+}
+
+
+let messageRef = push(ref(database,"messages"));
+
+
+set(messageRef,{
+name:name,
+text:text,
+time:Date.now()
+});
+
+
+document.getElementById("message").value="";
+
+};
+
+
+
+const chat = document.getElementById("chat");
+
+
+onValue(ref(database,"messages"),(snapshot)=>{
+
+
+chat.innerHTML="";
+
+
+snapshot.forEach((item)=>{
+
+
+let data=item.val();
+
+
+chat.innerHTML += `
+
+<div class="message">
+
+<b>${data.name}</b><br>
+
+${data.text}
+
+</div>
+
+`;
+
+});
+
+
+});
