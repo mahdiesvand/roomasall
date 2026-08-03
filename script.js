3203d80707e33a5a25cc2f82ff2e5fb1e9b26a59
mahdiesvand <mahdiesvand1020@gmail.com>
2026-08-03 14:25:21 +0330

    Remove Firebase setup and message handling code

--- script.js
+++ script.js
@@ -1,64 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
-
-const firebaseConfig = {
-  apiKey: "اینجا apiKey خودت",
-  authDomain: "اینجا authDomain خودت",
-  databaseURL: "اینجا databaseURL خودت",
-  projectId: "اینجا projectId خودت",
-  storageBucket: "اینجا storageBucket خودت",
-  messagingSenderId: "اینجا messagingSenderId خودت",
-  appId: "اینجا appId خودت"
-};
-
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-
-const messages = document.getElementById("messages");
-const online = document.getElementById("online");
-
-
-online.innerHTML = "متصل شد ✅";
-
-
-const messagesRef = ref(db, "messages");
-
-
-onValue(messagesRef, (snapshot)=>{
-
-messages.innerHTML="";
-
-snapshot.forEach((item)=>{
-
-let data=item.val();
-
-let div=document.createElement("div");
-div.className="msg";
-div.innerHTML=data.text;
-
-messages.appendChild(div);
-
-});
-
-});
-
-
-
 window.send = function(){
-alert("کار کرد");
-let input=document.getElementById("text");
 
-if(input.value.trim()=="") return;
+let text = document.getElementById("text").value;
 
+if(text == ""){
+alert("پیام خالی است");
+return;
+}
 
-push(messagesRef,{
-text:input.value
-});
+let box = document.getElementById("messages");
 
+let msg = document.createElement("div");
+msg.className = "msg";
+msg.innerHTML = text;
 
-input.value="";
+box.appendChild(msg);
+
+document.getElementById("text").value = "";
 
 }
