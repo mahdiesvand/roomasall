e4291077ee30912616d9ec45cffdf09ae0f8c250
mahdiesvand <mahdiesvand1020@gmail.com>
2026-08-03 13:06:25 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-function login(){
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmV0ikTOiXmtibjuXgJ4PhIbJcTxkhP5A",
+  authDomain: "roomasal.firebaseapp.com",
+  databaseURL: "https://roomasal-default-rtdb.firebaseio.com",
+  projectId: "roomasal",
+  storageBucket: "roomasal.firebasestorage.app",
+  messagingSenderId: "74039115594",
+  appId: "1:74039115594:web:18fb78cfe77207f18c4bb3"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+
+window.login = function(){
 
 let name = document.getElementById("username").value;
 
 
 if(name.trim()==""){
-
-alert("لطفاً نام کاربری وارد کنید");
-
+alert("نام کاربری وارد کنید");
 return;
-
 }
 
 
 localStorage.setItem("asalUser",name);
 
 
-window.location="chat.html";
+set(ref(db,"users/"+name),{
+username:name,
+online:true
+});
 
+
+window.location="chat.html";
 
 }
