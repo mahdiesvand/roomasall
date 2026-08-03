25ac52897f40b2de50a489bc93e377ea4ac80803
mahdiesvand <mahdiesvand1020@gmail.com>
2026-08-03 13:11:41 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+function login(){
 
+    let name = document.getElementById("username").value;
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBmV0ikTOiXmtibjuXgJ4PhIbJcTxkhP5A",
-  authDomain: "roomasal.firebaseapp.com",
-  databaseURL: "https://roomasal-default-rtdb.firebaseio.com",
-  projectId: "roomasal",
-  storageBucket: "roomasal.firebasestorage.app",
-  messagingSenderId: "74039115594",
-  appId: "1:74039115594:web:18fb78cfe77207f18c4bb3"
-};
+    if(name.trim() === ""){
+        alert("نام کاربری وارد کنید");
+        return;
+    }
 
+    localStorage.setItem("asalUser", name);
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-
-window.login = function(){
-
-let name = document.getElementById("username").value;
-
-
-if(name.trim()==""){
-alert("نام کاربری وارد کنید");
-return;
-}
-
-
-localStorage.setItem("asalUser",name);
-
-
-set(ref(db,"users/"+name),{
-username:name,
-online:true
-});
-
-
-window.location="chat.html";
+    window.location.href = "chat.html";
 
 }
