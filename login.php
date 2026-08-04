e2278d15bfb0ee5cd3b774dd720d982f673088b7
mahdiesvand <mahdiesvand1020@gmail.com>
2026-08-04 05:40:11 +0330

    Create login.php

---
+++ login.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title><?php echo(site_title); ?></title>
+		<meta content="<?php echo($options['chat_keywords']); ?>" name="keywords" />
+		<meta content="<?php echo($options['chat_description']); ?>" name="description" />
+		<meta name="distribution" content="global"/>
+		<meta name="robots" content="index, follow"/>
+		<meta name="revisit-after" content="1 days"/>
+		<meta name="rating" content="general"/>
+		<meta http-equiv="Cache-Control" content="no-cache" />
+		<meta http-equiv="Pragma" content="no-cache" />
+		<meta http-equiv="imagetoolbar" content="no" />
+		<link rel="stylesheet" href="<?php echo(theme::url('css/login.css')); ?>" type="text/css">
+		<?php theme::get_header( 'login'); ?>
+	</head>
+
+
+
+
+
+<body bgcolor="black">
+
+
+<div align="center">
+	<table border="0" width="100">
+		<tbody><tr>
+			<td><a title="&#1589;&#1601;&#1581;&#1607; &#1740; &#1575;&#1589;&#1604;&#1740;" target="_self" href="/"><img border="0" src="/theme/theme4/images/index.png" width="120" height="80" origsrc="/theme/theme4/images/index.png" title="&#1589;&#1601;&#1581;&#1607; &#1740; &#1575;&#1589;&#1604;&#1740;"></a></td>
+			<td rowspan="6"><img border="0" src="/theme/theme4/images/bg_headerout.png" width="793" height="488"></td>
+		</tr>
+		<tr>
+			<td>
+				<a title="&#1575;&#1605;&#1578;&#1740;&#1575;&#1586;&#1575;&#1578; &#1705;&#1575;&#1585;&#1576;&#1585;&#1575;&#1606;" target="_blank" href="/users/rank"><img border="0" src="/theme/theme4/images/top.png" width="120" height="68" origsrc="/theme/theme4/images/top.png" title="&#1575;&#1605;&#1578;&#1740;&#1575;&#1586;&#1575;&#1578; &#1705;&#1575;&#1585;&#1576;&#1585;&#1575;&#1606;"></a>
+	       	</td>
+		</tr>
+		<tr>
+			<td>
+				<a title="&#1662;&#1585;&#1608;&#1601;&#1575;&#1740;&#1604; &#1705;&#1575;&#1585;&#1576;&#1585;&#1575;&#1606;" target="_blank" href="/"><img border="0" src="/theme/theme4/images/profile.png" width="120" height="68" origsrc="/theme/theme4/images/profile.png" title="&#1662;&#1585;&#1608;&#1601;&#1575;&#1740;&#1604; &#1705;&#1575;&#1585;&#1576;&#1585;&#1575;&#1606;"></a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+			    <a title="&#1575;&#1585;&#1587;&#1575;&#1604; &#1578;&#1740;&#1705;&#1578; &#1576;&#1607; &#1605;&#1583;&#1740;&#1585;&#1740;&#1578;" target="_blank" href="/contact"><img border="0" src="/theme/theme4/images/us.png" width="120" height="68" origsrc="/theme/theme4/images/us.png" title="&#1575;&#1585;&#1587;&#1575;&#1604; &#1578;&#1740;&#1705;&#1578; &#1576;&#1607; &#1605;&#1583;&#1740;&#1585;&#1740;&#1578;"></a>
+			</td>
+		</tr>
+	</tbody></table>
+</div><hr width="1000" color="#CC0033" size="4">
+
+
+
+			<div align="center" style="position: absolute; top: 150px; left: 300px;">
+			
+<form id="form" onsubmit="return login(this);" action="" autocomplete="off">
+<div class="tf-wel">&#1608;&#1585;&#1608;&#1583; &#1576;&#1607; &#1670;&#1578; &#1585;&#1608;&#1605;</div>
+<div class="tf-user"><input type="text" name="username" class="tf-user2"  maxlength="20" placeholder="&#1606;&#1575;&#1605; &#1705;&#1575;&#1585;&#1576;&#1585;&#1740; ..."></div>
+<div class="tf-pass" id="lay_pw" style="display:none"><input type="password" class="tf-pass2" name="password" placeholder="&#1585;&#1605;&#1586; &#1593;&#1576;&#1608;&#1585; ..."></div>
+<div class="jens" id="lay_gender"><div class="switch switch-yellow">
+			  <input type="radio" class="switch-input" name="gender" value="1" id="week3" checked>
+			  <label for="week3" class="switch-label switch-label-off">&#1662;&#1587;&#1585;</label>
+			  <input type="radio" class="switch-input" name="gender" value="2" id="month3">
+			  <label for="month3" class="switch-label switch-label-on">&#1583;&#1582;&#1578;&#1585;</label>
+			  <span class="switch-selection"></span>
+			</div></div>
+<div id="lay_hide" style="display:none"><font>&#1608;&#1585;&#1608;&#1583; &#1576;&#1607; &#1589;&#1608;&#1585;&#1578; &#1662;&#1606;&#1607;&#1575;&#1606; :</font><input type="checkbox" name="hide" value="1" /></div>
+<input class="btn2" value="" onclick="window.location='/#;" style="cursor: pointer;position: absolute; margin-right: 103px;" type="submit">
+<input class="btn" value="" name="go" style="margin-left: 104px;cursor: pointer;" type="submit">
+						</form>
+						
+</div>
+<center>
+<a href="http://new4etchat.rzb.ir" target="_blank">
+<img class="img_button" border="0" src="http://www.axgig.com/images/96301667294657099732.png"></a><BR>
+</center>
+
+</html>
+
