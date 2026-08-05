64d8ae9407be017a63060af0b8618502d682909c
mahdiesvand <mahdiesvand1020@gmail.com>
2026-08-05 07:37:43 +0330

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-database.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmV0ikTOiXmtXjG4PhIbJcTxkhP5A",
+  authDomain: "roomasal.firebaseapp.com",
+  databaseURL: "https://roomasal-default-rtdb.firebaseio.com",
+  projectId: "roomasal",
+  storageBucket: "roomasal.firebasestorage.app",
+  messagingSenderId: "74039115594",
+  appId: "1:74039115594:web:18fb78cfe77207f18c4bb3"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+
+export const db = getDatabase(app);
