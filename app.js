1b9649b87a4e9bef8d7f5588d707f4f9255db4b9
mahdiesvand <mahdiesvand1020@gmail.com>
2026-08-05 07:39:07 +0330

    Implement messaging functionality with Firebase

---
+++ app.js
@@ -0,0 +1,74 @@
+import { db } from "./firebase.js";
+
+import {
+    ref,
+    push,
+    onChildAdded
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-database.js";
+
+
+const messagesRef = ref(db, "messages");
+
+
+let user = localStorage.getItem("username") || "کاربر";
+
+
+// ارسال پیام
+
+window.sendMessage = function(){
+
+    let input = document.getElementById("text");
+
+    let message = input.value.trim();
+
+
+    if(message === ""){
+        return;
+    }
+
+
+    push(messagesRef, {
+
+        name: user,
+
+        text: message,
+
+        time: Date.now()
+
+    });
+
+
+    input.value="";
+
+};
+
+
+
+// دریافت پیام‌ها
+
+onChildAdded(messagesRef, (data)=>{
+
+
+    let msg = data.val();
+
+
+    let box = document.getElementById("messages");
+
+
+    box.innerHTML += `
+
+    <div class="message">
+
+    <b>${msg.name}</b><br>
+
+    ${msg.text}
+
+    </div>
+
+    `;
+
+
+    box.scrollTop = box.scrollHeight;
+
+
+});
